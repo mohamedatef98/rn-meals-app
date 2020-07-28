9672b674e4bbe4a1b6a2f72e504da9a04509081a
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-28 11:25:03 +0200

    Display Categories in Grid in CategoriesList Screen

--- screens/CategoriesList.js
+++ screens/CategoriesList.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, FlatList } from 'react-native'
+
+import { CATEGORIES } from '../data/dummy-data'
 
 const CategoriesList = props => (
     <View>
-        <Text>The CategoriesList Screen</Text>
+        <FlatList
+            data={CATEGORIES}
+            numColumns={2}
+            renderItem={({ item }) => <View style={styles.gridItem}>
+                <Text>{item.title}</Text>
+            </View>}
+        />
     </View>
 )
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
+    }
+})
 
 export default CategoriesList
