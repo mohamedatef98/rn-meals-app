373d592449b0f01b68b7c4e410c07c895f14b4a8
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-07-29 22:12:13 +0200

    Add Icons For TabsNavigator Tabs

--- navigators/Tabs.js
+++ navigators/Tabs.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import StackNavigator from './Stack'
 import { Favorites } from '../screens'
+import { Colors } from '../theme'
 
 const Tabs = createBottomTabNavigator()
 
 const TabsNavigator = props => {
-    return <Tabs.Navigator>
+    return <Tabs.Navigator
+            tabBarOptions={{
+                activeTintColor: Colors.accent
+            }}
+        >
         <Tabs.Screen
             name='Meals'
             component={StackNavigator}
+            options={{
+                tabBarIcon: ({ color, size }) => <Ionicons name='ios-restaurant' size={size} color={color} />
+            }}
         />
         <Tabs.Screen
             name='Favorites'
             component={Favorites}
+            options={{
+                tabBarIcon: ({ color, size }) => <Ionicons name='ios-star' size={size} color={color} />
+            }}
         />
     </Tabs.Navigator>
 }
