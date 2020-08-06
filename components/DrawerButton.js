d4e06f2b018165eeaa0f00ca3a7e4017bb54ba28
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-07 01:07:17 +0200

    Use Touchable Component with DrawerButton

--- components/DrawerButton.js
+++ components/DrawerButton.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { TouchableOpacity, StyleSheet } from 'react-native'
+import { StyleSheet, Platform, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
+import Touchable from './Touchable'
+
 const DrawerButton = ({ navigation }) => {
-    return <TouchableOpacity style={styles.container} onPress={() => navigation.openDrawer()}>
-        <Ionicons name='ios-menu' size={30} color='white' />
-    </TouchableOpacity>
+    return <View style={styles.container}>
+        <Touchable onPress={() => navigation.openDrawer()} activeOpacity={0.8}>
+            <View style={styles.icon}>
+                <Ionicons name='ios-menu' size={30} color={Platform.OS === 'ios' ? 'black' : 'white'} />
+            </View>
+        </Touchable>
+    </View>
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginLeft: 20
+        marginLeft: 20,
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
+    icon: {
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
