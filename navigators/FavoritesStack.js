7d4cd1afe364ac886c51569e3a08866738cbf1cc
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-06 17:31:38 +0200

    Add Favorite Meal Screen to display single Favorite Meal

--- navigators/FavoritesStack.js
+++ navigators/FavoritesStack.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import { Favorites } from '../screens'
+import { Favorites, MealDetails } from '../screens'
 import { Colors } from '../theme'
 
 import sharedScreenOptions from './stackSharedScreenOptions'
@@ -11,6 +11,10 @@ const Stack = createStackNavigator()
 const FavoritesStack = props => {
     return <Stack.Navigator screenOptions={sharedScreenOptions(Colors.accent)}>
         <Stack.Screen component={Favorites} name='Your Favorites' />
+        <Stack.Screen
+            component={MealDetails}
+            name={'FavoriteMeal'}
+            options={({ route }) => ({ title: route?.params?.meal.title })} />
     </Stack.Navigator>
 }
 
