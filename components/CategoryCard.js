d49bbfdb1d96fccd5c5cc03236c26d3e1f7a03ff
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-14 03:02:32 +0200

    Add CategoryCard shadow

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -6,22 +6,36 @@ import { Fonts } from '../theme'
 const CategoryCard = ({ category, onPress }) => {
     const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
 
-    return <View style={styles.categoryContainer}>
-        <Touchable onPress={onPress} style={styles.touchable}>
-            <View style={[styles.gridItem, { backgroundColor: category.color }]}>
-                <Text style={styles.text} numberOfLines={2}>{category.title}</Text>
-            </View>
-        </Touchable>
+    return <View style={styles.container}>
+        <View style={styles.categoryContainer}>
+            <Touchable onPress={onPress} style={styles.touchable}>
+                <View style={[styles.gridItem, { backgroundColor: category.color }]}>
+                    <Text style={styles.text} numberOfLines={2}>{category.title}</Text>
+                </View>
+            </Touchable>
+        </View>
     </View>
 }
 
 const styles = StyleSheet.create({
-    categoryContainer: {
+    container: {
         flex: 1,
         height: 150,
         margin: 15,
         borderRadius: 10,
-        overflow: 'hidden'
+        shadowColor: '#ccc',
+        shadowOffset: {
+            width: 10,
+            height: 10
+        },
+        shadowRadius: 10,
+        shadowOpacity: 0.8,
+        elevation: 8
+    },
+    categoryContainer: {
+        overflow: 'hidden',
+        borderRadius: 10,
+        flex: 1
     },
     touchable: {
         flex: 1
