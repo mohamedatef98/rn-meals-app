b14ce61069068ff937dfd4ee04648e1e3bfd043b
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-14 03:26:25 +0200

    Add Filters To Filters Screen

--- screens/Filters.js
+++ screens/Filters.js
@@ -1,12 +1,62 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
 
-const Filters = props => (
-    <View>
-        <Text>The Filters Screen</Text>
+import { FilterControl } from '../components'
+import { Fonts } from '../theme'
+
+const Filters = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false)
+    const [isLactoseFree, setIsLactoseFree] = useState(false)
+    const [isVegan, setIsVegan] = useState(false)
+    const [isVegetaran, setIsVegetaran] = useState(false)
+
+    return <View style={styles.screen}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Filters</Text>
+            <FilterControl
+                label='Gluten-Free'
+                value={isGlutenFree}
+                onFilterChange={setIsGlutenFree}
+                style={styles.filter}
+            />
+            <FilterControl
+                label='Lactose-Free'
+                value={isLactoseFree}
+                onFilterChange={setIsLactoseFree}
+                style={styles.filter}
+            />
+            <FilterControl
+                label='Vegan'
+                value={isVegan}
+                onFilterChange={setIsVegan}
+                style={styles.filter}
+            />
+            <FilterControl
+                label='Vegetaran'
+                value={isVegetaran}
+                onFilterChange={setIsVegetaran}
+                style={styles.filter}
+            />
+        </View>
     </View>
-)
+}
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    container: {
+        width: '80%'
+    },
+    title: {
+        fontFamily: Fonts.bold,
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    filter: {
+        marginTop: 10
+    }
+})
 
 export default Filters
