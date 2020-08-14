99e83894bd571a5654bd3e75fc22aef85a10b9b9
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-14 07:15:42 +0200

    Add ToggleFavorite Button to Favorite Meal Screen

--- navigators/FavoritesStack.js
+++ navigators/FavoritesStack.js
@@ -1,27 +1,39 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import { Favorites, MealDetails } from '../screens'
-import { DrawerButton } from '../components'
+import { DrawerButton, HeaderButtonCreator, HeaderButtonBase } from '../components'
 import { Colors } from '../theme'
 
 import sharedScreenOptions from './stackSharedScreenOptions'
+import { toggleFavorite } from '../store'
 
 const Stack = createStackNavigator()
 
 const FavoritesStack = ({ navigation }) => {
+    const dispatch = useDispatch()
+    const favMeals = useSelector(state => state.meals.favoriteMeals)
+
     return <Stack.Navigator screenOptions={sharedScreenOptions(Colors.accent)}>
         <Stack.Screen
             component={Favorites}
             name='Your Favorites'
-            options={{ 
+            options={{
                 headerLeft: () => <DrawerButton navigation={navigation} />
-            }}    
+            }}
         />
         <Stack.Screen
             component={MealDetails}
             name={'FavoriteMeal'}
-            options={({ route }) => ({ title: route?.params?.meal.title })} />
+            options={({ route }) => ({
+                title: route?.params?.meal?.title,
+                headerRight: HeaderButtonCreator(HeaderButtonBase, {
+                    onPress: () => dispatch(toggleFavorite(route?.params?.meal)),
+                    iconName: favMeals.includes(route?.params?.meal) ? 'ios-star' : 'ios-star-outline' 
+                })
+            })}
+        />
     </Stack.Navigator>
 }
 
