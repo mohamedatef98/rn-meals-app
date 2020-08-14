424157b0e66c628e81100c966a6d2fd1a5b3585e
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-14 07:23:26 +0200

    Add Text if Favorite Meals are Zero

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { useSelector } from 'react-redux'
 
 import { MealList } from '../components'
+import { Fonts } from '../theme'
 
 const Favorites = ({ navigation }) => {
     const meals = useSelector(state => state.meals.favoriteMeals)
 
+    if (meals.length < 1)
+        return <View style={styles.screen}>
+            <Text style={styles.text}>No favorite meals found.</Text>
+            <Text style={styles.text}>Start adding some!</Text>
+        </View>
+
     return <MealList meals={meals} onMealSelected={meal => navigation.navigate('FavoriteMeal', { meal })} />
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontFamily: Fonts.bold,
+        fontSize: 15
+    }
+})
 
 export default Favorites
